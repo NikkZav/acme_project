0f0298ac89faa540f0d4db97e54381c7f7870ae0
Nikita Zavyalov <nikitazavyalov.it@gmail.com>
2024-11-09 23:14:20 +0300

    Отправка писем. Эмуляция почтового сервера

--- acme_project/pages/views.py
+++ acme_project/pages/views.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView
 
 from birthday.models import Birthday
 
+from django.contrib.auth import get_user_model
+
 
 class HomePage(TemplateView):
     template_name = 'pages/index.html'
@@ -14,5 +16,20 @@ class HomePage(TemplateView):
         # Добавляем в словарь ключ total_count;
         # значение ключа — число объектов модели Birthday.
         context['total_count'] = Birthday.objects.count()
+
+        # Получаем модель, зарегистрированную в конфиге проекта,
+        # в константе AUTH_USER_MODEL
+        User = get_user_model()
+
+        # И в коде применяем значение переменной User, 
+        # которое вернула функция get_user_model():
+        users = User.objects.all()
+        context['users'] = users
+
+        user = self.request.user
+        user.refresh_from_db()
+        print(user.username)
+        print(user.user_permissions.all())
+
         # Возвращаем изменённый словарь контекста.
         return context
